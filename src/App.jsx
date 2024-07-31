0cdde32bf368e5c8a02028a8290e31ef1ea1ea63
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-07-31 18:35:27 +0200

    Sidebar, NewProject and NoProjectSelected components setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import Sidebar from "./components/Sidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectsState(prevProjectState => {
+      console.log("cliccato")
+      return{
+        ...prevProjectState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  } else if(projectsState.selectedProjectId === null){
+    content = <NewProject />
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar onStartAddProject={handleStartAddProject}/>
+      {content}
+    </main>
   );
 }
 
